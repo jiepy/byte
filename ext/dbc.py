0ade382d99bf9095ad729a81639576bff6ddfdea
Geng Jie <baiyaozi@7lk.com>
2016-02-27 22:25:18 +0800

    modify dbc.py

--- ext/dbc.py
+++ ext/dbc.py
@@ -4,7 +4,6 @@ import pymysql
 
 #设置数据库地址
 DB_host = '192.168.214.88'
-
 #设置数据库名称
 DB_name = 'testDB'
 #必须不加引号
@@ -13,7 +12,6 @@ DB_port = 3306
 DB_user = 'test'
 #设置密码
 DB_pass = 'test123'
-
 #定义数据库信息
 conn = pymysql.connect(host=DB_host,
                        port=DB_port, user=DB_user,
@@ -23,10 +21,8 @@ cur = conn.cursor()
 #执行sql语句
 cur.execute('SELECT * from students')
 
-
 for i in cur.fetchall():
 	print(i)
-
 #关闭游标
 conn.close()
 
